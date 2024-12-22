b6cfe95045d00139a457ef9d7a4405f6b5bf5451
kamranikramofficial <kamranikram706@gmail.com>
2024-12-23 00:00:22 +0500

    first

--- js1st/first assignment/script.js
+++ js1st/first assignment/script.js
@@ -1,15 +1,15 @@
-let hello =("hellow")
-hello+=(" word")
+let hello ="hellow"
+hello+=" word"
 alert( hello)
  
-var hel =("js")
-hel=("Enter your corect email")
+var hel ="js"
+hel="Enter your corect email"
 alert ( hel)
  
-const ka =("Enter your corect password")
+const ka ="Enter your corect password"
 alert (ka)
 
- let thanks=("Thank you")
+ let thanks="Thank you"
  alert (thanks)
 console .log=(thanks)
 
