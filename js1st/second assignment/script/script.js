70cdd0b7e292462ac6983ebeff9cdcb821e9fba2
kamranikramofficial <kamranikram706@gmail.com>
2024-12-22 23:50:37 +0500

    add secocond

---
+++ js1st/second assignment/script/script.js
@@ -0,0 +1,28 @@
+let message = "Hello World";
+let username = "Kamran ikram";
+let age = 17;
+let email = "kamranikram706@gmail.com";
+
+alert(message)
+alert("My Name is " + username)
+alert("I am " + age + " years old")
+alert("My Email is " + email)
+
+let completeMessage = `My Name is ${username}, I am ${age} years old, and My Email is ${email}`
+
+alert(completeMessage)
+
+let pizza = ` Pizza \n Pizz \n Piz \n Pi \n P`
+
+alert(pizza)
+
+
+let book = "A smarter way to learn JavaScript";
+
+alert(`I am trying To Learn From Book "${book}". `)
+
+
+
+document.write("This is my first massage using js")
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
