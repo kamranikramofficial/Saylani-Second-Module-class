e07a89ecfcfd0e858ce7ce5e870dcfc65d02566f
kamranikramofficial <kamranikramofficial@gmail.com>
2024-12-22 20:52:03 +0500

    first js assig

---
+++ js1st/first assignment/script.js
@@ -0,0 +1,15 @@
+let hello =("hellow")
+hello+=(" word")
+alert( hello)
+ 
+var hel =("js")
+hel=("Enter your corect email")
+alert ( hel)
+ 
+const ka =("Enter your corect password")
+alert (ka)
+
+ let thanks=("Thank you")
+ alert (thanks)
+console .log=(thanks)
+
