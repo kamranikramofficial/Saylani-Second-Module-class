0996698ae2edcba6383fc9b94467436beec80379
kamranikramofficial <kamranikram706@gmail.com>
2024-12-24 00:21:18 +0500

    fifth assigment

---
+++ js1st/fifthassignment/secript/script.js
@@ -0,0 +1,14 @@
+
+let prom = prompt("enter your value");
+let num = 0;
+
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
+document.write(prom * ++num + "<br>");
