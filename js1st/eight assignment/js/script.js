fa747efd477aa84b70ed578f67eb5ade988d23dc
kamranikramofficial <kamranikram706@gmail.com>
2025-01-02 23:15:34 +0500

    add array

---
+++ js1st/eight assignment/js/script.js
@@ -0,0 +1,55 @@
+alert("welcome to learn Array")
+//array varivabal decleyer simple mathord
+let city = ["karachi", "panjab", "multan", "lahore"];
+console.log([city])
+console.log(city[0])
+console.log(city[3])
+
+// array declure value change
+let opra = ["karachi", "panjab", "multan", "lahore"];
+opra[2] = "pindi";
+console.log(opra[0]);
+
+// array declure with different data type
+let diff = ["karachi", 1, true,]
+console.log(diff[1])
+
+// array in pop method
+let pope = ["karachi", "panjab", "multan", "lahore"];
+pope.pop();
+console.log(pope)
+
+
+
+//array in shift method remove first element in array add this is same of pop method but this is remove first element
+
+let shish = ["karachi", "panjab", "multan", "lahore"];
+shish.shift();
+console.log(shish)
+
+
+// array in pop method remove two than increse pop commad and remove more  element
+let poper = ["karachi", "panjab", "multan", "lahore"];
+poper.pop();
+poper.pop();
+console.log(poper
+
+
+)
+//array in shift method remove first element in array add this is same of pop method but this is remove first element
+
+let shis = ["karachi", "panjab", "multan", "lahore"];
+shis.shift();
+shis.shift();
+console.log(shis)
+
+
+// array in push method add onei or more element in array 
+let pets = ["dog", "cat", "bird"];
+pets.push("fish", "gerbil");
+console.log(pets)
+
+//aaray in unshift method add one or more element in array add this is same of push method but this is add first element
+let pets1 = ["dog", "cat", "bird"];
+pets1.unshift(["fish", "gerbil"])
+console.log(pets1)
