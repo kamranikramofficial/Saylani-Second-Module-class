31f4a5008b2364fcd34877eee9333ddf476f2986
kamranikramofficial <kamranikramofficial@gmail.com>
2025-01-14 23:41:50 +0500

    14-1-2025

---
+++ js1st/sixten assignment/files/app.js
@@ -0,0 +1,40 @@
+// let namn =["kamran" , "ikram","ahmad" ,"ammar"]
+// let plan=prompt("enter your pladelarem name");
+// let boo=false;
+// for (var i=0 ;i<= namn.length -1 ;i++){
+//      if(plan==namn[i]){
+//         boo=true;
+//         break; 
+//      }
+// }
+
+// if(boo == true){
+//     console.log("found")
+// }else{
+//     console.log("Not found")
+// }
+
+// let nam = ["kamran", "ikram", "ahmad", "ammar"];
+
+// let longestName = "";
+
+// for (let i = 0; i < nam.length; i++) {
+//     if (nam[i].length > longestName.length) {
+//         longestName = nam[i];
+//     }
+// }
+
+// console.log("The longest name is: " + longestName);
+
+
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=5;j++){
+    }
+    if(i==1 && i==5 && j==1 && j==3){
+        document.write("*")
+    }else{
+        document.write(" ")
+    }
+    document.write("<br>")
+}
+
