cc0c90d432009a28eaf7e2c3be8e6b168a99af21
kamranikramofficial <kamranikram706@gmail.com>
2025-01-16 04:28:15 +0500

    and name lenth chek and num

--- js1st/seventen assignment/files/app.js
+++ js1st/seventen assignment/files/app.js
@@ -9,3 +9,28 @@ if(reve.toLowerCase == inpu.toLowerCase){
 else{
     alert("not palindrome")
 }
+
+
+// small and large num valu code
+
+let arranum =[44,55,35,246,78,369,95,456,446];
+let largnum =0;
+let smallnum=1;
+for(let i=0; i<arranum.length; i++){
+    if(arranum[i] > largnum){
+        largnum = arranum[i];
+    }
+    if(arranum[i] < smallnum){
+        smallnum = arranum[i];
+    }
+}
+alert(`This is large number ${largnum}`)
+alert(`This is large small number ${smallnum}`)
+
+
+//chack your name Word lenth
+
+let nam = prompt("Enter your name");
+let leng = nam.length;
+alert(`Your name length is ${leng}`)
+
