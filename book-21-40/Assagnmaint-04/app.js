4b3960cd707575381f54313f8c62d20ffe3982b2
kamranikramofficial <kamranikram706@gmail.com>
2025-01-26 10:45:50 +0500

    book capter number 24

---
+++ book-21-40/Assagnmaint-04/app.js
@@ -0,0 +1,40 @@
+// CHAPTER-24: Finding a character at a location
+
+// METHOD WE'LL DISCUSS
+// charAt
+
+// returns the character on the given address
+var institute = "saylani mass it training";
+console.log(institute.charAt(4));
+
+// returns EMPTY STRING if index doesn't exist
+var institute = "saylani mass it training";
+console.log(institute.charAt(140));
+
+// CAN'T USE WITH ARRAYS
+var mohalla = ["ali", "ahmad", "akram", "aslam", "ajmal", "aslam"];
+alert(mohalla.charAt(3));
+
+var fruit = "apple";
+console.log(fruit.charAt(3));
+
+// IF U WANT TO ACCESS SPACE
+var greetings = "hi how are you?";
+console.log(greetings.charAt(2));
+
+// EMAIL VALIDATION USING CHARAT
+var userEmail = prompt("Please enter your email address");
+var isValid = false;
+
+for (var charIndex = 0; charIndex < userEmail.length; charIndex++) {
+    if (userEmail.charAt(charIndex) === "@") {
+        isValid = true;
+        break;
+    }
+}
+
+if (isValid) {
+    alert("Valid email address");
+} else {
+    alert("Invalid email address");
+}
