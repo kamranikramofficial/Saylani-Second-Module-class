5a4250396332d1a84bc7c70191fe658ff79b2f10
kamranikramofficial <kamranikramofficial@gmail.com>
2025-01-30 23:42:04 +0500

    book chapter number 28

---
+++ book-21-40/Assignment-08/app.js
@@ -0,0 +1,22 @@
+
+var currentAge = prompt("Enter your age.");
+var yearsEligibleToVote = currentAge - 18;
+
+var profit = "200" - "150";
+
+var profit = "200" - "duck";
+
+var result = "200" + 150;
+
+var currentAge = prompt("Enter your age.");
+var qualifyingAge = currentAge + 1;
+
+
+// tha parsefloat is used to desplay tha valu with Dasimal number
+var myFractional = parseFloat("1.9999");
+console.log(myFractional);//1.9999
+
+
+// tha parsefloat is used to desplay tha valu with out Dasimal number
+var myFractional = parseInt("1.9999");
+console.log(myFractional);//1
