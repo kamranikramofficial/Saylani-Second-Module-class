6e32be9a196b420fc377b90279ad325f9897affe
kamranikramofficial <kamranikram706@gmail.com>
2025-02-04 23:13:03 +0500

    book chapter 33

---
+++ book-21-40/Assignment-13/app.js
@@ -0,0 +1,29 @@
+// CHAPTER-33: SPECIFYING A DATE AND TIME
+
+var now = new Date();
+console.log(now);
+
+var helloToTheFuture = new Date("June 30, 2035");
+console.log(helloToTheFuture);
+
+// INVALID DATE
+var helloToTheFuture = new Date("June 33, 2035");
+console.log(helloToTheFuture);
+
+// DAYS TO MOSAM CHANGE
+var now = new Date();
+var mosamChange = new Date("November 8, 2024");
+console.log(mosamChange)
+var timeToMosamChangeInMilliseconds = mosamChange - now;
+console.log({ timeToMosamChangeInMilliseconds });
+var daysToMosamChange = timeToMosamChangeInMilliseconds / (1000 * 60 * 60 * 24);
+console.log(daysToMosamChange);
+console.log(Math.floor(daysToMosamChange));
+
+// HOURS TO MOSAM CHANGE
+var now = new Date();
+var mosamChange = new Date("November 8, 2024");
+var timeToMosamChangeInMilliseconds = mosamChange - now;
+var hoursToMosamChange = timeToMosamChangeInMilliseconds / (1000 * 60 * 60);
+console.log(hoursToMosamChange);
+console.log(Math.floor(hoursToMosamChange));
