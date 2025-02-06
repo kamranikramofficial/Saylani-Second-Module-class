b2c524539495f4cb6ca4db2ed8025236bd5b7099
kamranikramofficial <kamranikramofficial@gmail.com>
2025-02-06 23:32:22 +0500

    book chapter number 34

---
+++ book-21-40/Assignment-14/app.js
@@ -0,0 +1,26 @@
+
+// Chapter-34: CHANGING ELEMENTS OF A DATE AND TIME
+
+// OLD STUFF
+var now = new Date();
+console.log(now.getDay());
+
+// SET FULL YEAR
+var now = new Date();
+console.log({ now });
+console.log("before setting: ", now.getFullYear());
+
+// setting full year
+now.setFullYear(2035);
+console.log({ now });
+console.log("after setting: ", now.getFullYear());
+
+// SET HOURS
+// before setting
+console.log("before setting hours: ", now.getHours());
+
+// setting
+now.setHours(22);
+
+// after setting
+console.log("after setting hours: ", now.getHours());
