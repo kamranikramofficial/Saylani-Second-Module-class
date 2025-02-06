bf43ccd0b68a538af23bc2a0cca7599ddb08e3c7
kamranikramofficial <kamranikramofficial@gmail.com>
2025-02-06 23:47:24 +0500

    book chapter number 38

---
+++ book-21-40/Assignment-18/app.js
@@ -0,0 +1,19 @@
+//Chapter-38: Functions: Local vs. global variables
+
+// DEF: local vs global variable
+
+// Global Variable
+var globalVar = "I'm a global variable";
+
+function myFunction() {
+  // Local Variable
+  var localVar = "I'm a local variable";
+
+  console.log("Inside Function: ", globalVar);
+  console.log("Inside Function: ", localVar);
+}
+
+// myFunction();
+
+console.log("Outside Function:", globalVar);
+console.log("Outside Function:", localVar);
